29dd5cea43dbb7914125bdd7943d1187523db380
Gemagit <gemitaphotographer@gmail.com>
2024-03-11 18:30:00 +0100

    respuesta checked y contador

--- script.js
+++ script.js
@@ -36,23 +36,23 @@ localStorage.setItem("getDataUsers", dataAnswers);
 async function print1stQuestion() {
     let response = await fetch("https://opentdb.com/api.php?amount=1&category=31&difficulty=easy&type=multiple");
     let data = await response.json();
-    /* let data = {
-        "response_code": 0,
-        "results": [
-            {
-                "type": "multiple",
-                "difficulty": "easy",
-                "category": "Entertainment: Japanese Anime &amp; Manga",
-                "question": "In &quot;To Love-Ru&quot;, Ren and Run are from what planet?",
-                "correct_answer": "Memorze",
-                "incorrect_answers": [
-                    "Deviluke",
-                    "Mistletoe",
-                    "Okiwana"
-                ]
-            }
-        ]
-    }; */
+    /*  let data = {
+         "response_code": 0,
+         "results": [
+             {
+                 "type": "multiple",
+                 "difficulty": "easy",
+                 "category": "Entertainment: Japanese Anime &amp; Manga",
+                 "question": "In &quot;To Love-Ru&quot;, Ren and Run are from what planet?",
+                 "correct_answer": "Memorze",
+                 "incorrect_answers": [
+                     "Deviluke",
+                     "Mistletoe",
+                     "Okiwana"
+                 ]
+             }
+         ]
+     }; */
     let question = data.results[0].question;
     let correctAnswer = data.results[0].correct_answer; // --> La respuesta correcta sigue estando aquí
     let allAnswers = data.results[0].incorrect_answers; // --> Array con tres elementos
@@ -78,23 +78,23 @@ document.querySelector("button").addEventListener("click", async function (event
     event.preventDefault();
     let response = await fetch("https://opentdb.com/api.php?amount=1&category=31&difficulty=easy&type=multiple");
     let data = await response.json();
-    /* let data = {
-        "response_code": 0,
-        "results": [
-            {
-                "type": "multiple",
-                "difficulty": "easy",
-                "category": "Entertainment: Japanese Anime &amp; Manga",
-                "question": "In &quot;To Love-Ru&quot;, Ren and Run are from what planet?",
-                "correct_answer": "Memorze",
-                "incorrect_answers": [
-                    "Deviluke",
-                    "Mistletoe",
-                    "Okiwana"
-                ]
-            }
-        ]
-    }; */
+    /*   let data = {
+          "response_code": 0,
+          "results": [
+              {
+                  "type": "multiple",
+                  "difficulty": "easy",
+                  "category": "Entertainment: Japanese Anime &amp; Manga",
+                  "question": "In &quot;To Love-Ru&quot;, Ren and Run are from what planet?",
+                  "correct_answer": "Memorze",
+                  "incorrect_answers": [
+                      "Deviluke",
+                      "Mistletoe",
+                      "Okiwana"
+                  ]
+              }
+          ]
+      }; */
 
     let question = data.results[0].question;
     let correctAnswer = data.results[0].correct_answer; // --> La respuesta correcta sigue estando aquí
@@ -116,6 +116,104 @@ document.querySelector("button").addEventListener("click", async function (event
     document.getElementById("question").innerHTML = question;
 });
 
+
+//SELECCIONAR CON BOTON LA RESPUESTA ELEGIDA Y CAMBIARLA DE COLOR
+//Aquí recojo todos los inputs y los meto en una variable
+const arrayInputs = document.querySelectorAll("input");
+//el evento input hace que se produzca un cambio cada vez que el usuario modifique algo, es decir cada vez que cambie el valor
+// recorro todos los input 
+arrayInputs.forEach(function (input) {
+    //le digo que este pendiente cada vez que el usuario haga input
+    input.addEventListener("input", function (event) {
+        //el evento input se acciona cuando el usuario hace cualquier cambio,cualquier cambio a cualquier valor
+        //si checked es false
+        arrayInputs.forEach(
+            function (input) {
+                //recorro de nuevo todos los inputs, los reviso y los pinto de color oscuro si estan checked
+                if (input.checked) {
+                    input.parentElement.style.backgroundColor = "black";
+                } else {
+                    input.parentElement.style.backgroundColor = " rgba(241, 243, 244, 0.5)";
+                }
+                //y ahora pintamelos claritos si no esta chequeado
+            })
+    });
+});
+
+//AVISO DE RESPUESTAS NO CHEQUEADAS
+
+let submitButton = document.querySelector("button")
+
+submitButton.addEventListener("click", function (event) {
+    let counterRespondidas = 0;
+    let arrayInputs = document.querySelectorAll("input")
+    arrayInputs.forEach(function (input) {
+        if (input.checked) {
+            counterRespondidas++;
+        }
+    })
+    if (counterRespondidas != 10) {
+        alert('Debes seleccionar alguna respuesta')
+    }
+})
+
+//HACER CONTADOR PARA PREVIAMENTE IMPRIMIR UN <p> (appendchild) y añadirle resultado con mensaje
+
+//he accedido al formulario y le he creado el evento preventDefault para que cuando el usuario pulse submit tenga en cuenta una serie de condiciones antes de comprobar los aciertos
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    console.log(event);
+    console.log(event.target.pregunta1);
+    const respuesta1 = event.target.pregunta1.value;
+    const respuesta2 = event.target.pregunta2.value;
+    const respuesta3 = event.target.pregunta3.value;
+    const respuesta4 = event.target.pregunta4.value;
+    const respuesta5 = event.target.pregunta5.value;
+    const respuesta6 = event.target.pregunta1.value;
+    const respuesta7 = event.target.pregunta2.value;
+    const respuesta8 = event.target.pregunta3.value;
+    const respuesta9 = event.target.pregunta4.value;
+    const respuesta10 = event.target.pregunta5.value;
+
+
+
+    let aciertos = 0;
+    if (respuesta1 === respuestasCorrectas.pregunta1) aciertos++;
+    if (respuesta2 === respuestasCorrectas.pregunta2) aciertos++;
+    if (respuesta3 === respuestasCorrectas.pregunta3) aciertos++;
+    if (respuesta4 === respuestasCorrectas.pregunta4) aciertos++;
+    if (respuesta5 === respuestasCorrectas.pregunta5) aciertos++;
+    if (respuesta6 === respuestasCorrectas.pregunta6) aciertos++;
+    if (respuesta7 === respuestasCorrectas.pregunta7) aciertos++;
+    if (respuesta8 === respuestasCorrectas.pregunta8) aciertos++;
+    if (respuesta9 === respuestasCorrectas.pregunta9) aciertos++;
+    if (respuesta10 === respuestasCorrectas.pregunta10) aciertos++;
+
+    let msj = "";
+
+
+    if (aciertos <= 5) {
+        msj += `Número de aciertos: ${aciertos}. \n Por favor, lea la documentación rockera.n`;
+
+    } else { //4 aciertos o más
+        msj += `Número de aciertos: ${aciertos}. \n ¡Enhorabuena, estás hecho un auténtico rockero!\n`;
+        event.target.submit();
+    }
+
+    let p = document.createElement("pre");
+    let mensaje = document.createTextNode(msj);
+    p.style.color = "#DD1C1A";
+    p.style.fontSize = "16px";
+    p.appendChild(mensaje);
+
+    document.getElementsByTagName("form").appendChild(p); // dibuja resultado
+
+});
+
+
+
+
 /*GRÁFICA PIE PORCENTAJE
 
 var data = {
@@ -129,5 +227,17 @@ new Chartist.Pie('.ct-chart', data, {
     return Math.round(value / data.series.reduce(sum) * 100) + '%';
   }
 });
-*/
+;*/
+
+
+
+
+
+
+
+
+
+
+
+
 
